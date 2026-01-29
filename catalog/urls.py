d2358c56c194a32b71d1bf1a690d1ec020be0c10
Александр <ryasantsev.a.r@gmail.com>
2026-01-29 19:18:04 +0300

    новый контроллер и шаблон для отображения
    код для отображения списка товаров с помощью цикла
    
    создайте подшаблон для главного меню

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact
+from catalog.views import home, contact, product_detail, catalog
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
+    path("catalog/", catalog, name="catalog"),
     path("contacts/", contact, name="contact"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
 ]
